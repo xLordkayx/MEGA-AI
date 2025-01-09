f9b278806e79744dd30d036c9e23bce0c92fbe39
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2025-01-10 02:42:39 +0500

    MEGA-AI

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const defaultOwner = '923444844060';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || ;  // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '' ; // put your number here
 
 const ownerlist = ownervb.split(';');
 
@@ -20,8 +20,8 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'MEGA-AI';
-global.pairingNumber = process.env.BOT_NUMBER || ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || ;  // put your session id here
+global.pairingNumber = process.env.BOT_NUMBER || '' ;  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
 
 global.mods = []
 global.prems = []
